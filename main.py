7e921747983eb01a6bc73e8391d9ec500eca7c0b
Samuel-Brisio <samuelbrisio@outlook.com>
2026-09-04 15:06:33 -0300

    6. Executando Todos os Testes

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from TestFramework.TestLoader import TestLoader
-from Test.TestLoaderTest import TestLoaderTest
+from TestFramework.TestSuite import TestSuite
 from TestFramework.TestRunner import TestRunner
+
+from Test.TestLoaderTest import TestLoaderTest
+from Test.TestSuiteTest import TestSuiteTest
+from Test.TestCaseTest import TestCaseTest
 
 if __name__ == "__main__":
     loader = TestLoader()
-    suite = loader.make_suite(TestLoaderTest)
+    test_case_suite = loader.make_suite(TestCaseTest)
+    test_suite_suite = loader.make_suite(TestSuiteTest)
+    test_load_suite = loader.make_suite(TestLoaderTest)
+
+    suite = TestSuite()
+    suite.add_test(test_case_suite)
+    suite.add_test(test_suite_suite)
+    suite.add_test(test_load_suite)
 
     runner = TestRunner()
     runner.run(suite)
