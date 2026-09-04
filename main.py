b14c66c6cf14033cf66d437abf9284e8f0f5b625
Samuel-Brisio <samuelbrisio@outlook.com>
2026-09-04 15:02:15 -0300

    5. Classes TestLoader e TestRunner

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-from Test.Test import TestSuiteTest, TestCaseTest
-from TestFramework.TestSuite import TestSuite
-from TestFramework.TestResult import TestResult
+from TestFramework.TestLoader import TestLoader
+from Test.TestLoaderTest import TestLoaderTest
+from TestFramework.TestRunner import TestRunner
 
 if __name__ == "__main__":
-    result = TestResult()
-    suite = TestSuite()
+    loader = TestLoader()
+    suite = loader.make_suite(TestLoaderTest)
 
-    suite.add_test(TestCaseTest('test_result_success_run'))
-    suite.add_test(TestCaseTest('test_result_failure_run'))
-    suite.add_test(TestCaseTest('test_result_error_run'))
-    suite.add_test(TestCaseTest('test_result_multiple_run'))
-    suite.add_test(TestCaseTest('test_was_set_up'))
-    suite.add_test(TestCaseTest('test_was_run'))
-    suite.add_test(TestCaseTest('test_was_tear_down'))
-    suite.add_test(TestCaseTest('test_template_method'))
-
-    suite.add_test(TestSuiteTest('test_suite_size'))
-    suite.add_test(TestSuiteTest('test_suite_success_run'))
-    suite.add_test(TestSuiteTest('test_suite_multiple_run'))
-
-    suite.run(result)
-
-
-    print(result.summary())
+    runner = TestRunner()
+    runner.run(suite)
